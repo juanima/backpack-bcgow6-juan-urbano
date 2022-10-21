b55aa282b86e81df378780f129cef2e3fb9e7e1a
juanima <juan.urbano@mercadolibre.com.co>
2022-10-20 23:51:50 -0500

    preparar condiciones previas a los cambios

---
+++ 0x13-testing_integration_test/ejercicio_1/README.md
@@ -0,0 +1,78 @@
+# Ejercicio 1
+
+## Enunciated *_Service/Repo/Db Update()_*
+
+Diseñar un test que pruebe en la capa service, el método o función Update(). Para lograrlo se deberá:
+
+1. Crear un mock de Storage, dicho mock debe contener en su data un producto con las especificaciones que desee.
+2. El método Read del Mock, debe contener una lógica que permita comprobar que dicho método fue invocado. 
+3. Para dar el test como OK debe validarse que al invocar el método del Service Update(),  retorne el producto con mismo Id y los datos actualizados. Validar también que  Read() del Store haya sido ejecutado durante el test. 
+
+
+## Estructura de directorios
+
+```bash
+$ tree
+.
+├── README.md
+├── cmd
+│   └── server
+│       ├── handler
+│       │   ├── handler.go
+│       │   └── middleware.go
+│       └── main.go
+├── create_products.json
+├── docs
+│   ├── docs.go
+│   ├── swagger.json
+│   └── swagger.yaml
+├── go.mod
+├── go.sum
+├── images
+│   └── swagger-web.png
+├── internal
+│   ├── domain
+│   │   └── product.go
+│   └── products
+│       ├── repository.go
+│       ├── repository_mock_test.go
+│       ├── repository_stub_test.go
+│       └── service.go
+├── patch_products.json
+├── pkg
+│   ├── store
+│   │   ├── file.go
+│   │   ├── mock_file.go
+│   │   └── stub_file.go
+│   └── web
+│       └── response.go
+├── products.json
+└── update_products.json
+
+11 directories, 23 files
+```
+
+> Los test los puedes encontrar siguiendo `repository_mock_test.go` y `mock_file.go`
+
+>> `mock_file.go`: creamos una estructura que implementa los metodos del repository original, con esto logramos crear una instancia del struct `MockStoreFile` con ello podemos inyectarlo al repository
+
+>> `repository_mock_test.go`: creamos los test para probar el método `UpdateName()` y que utiliza el *mock* de store
+
+### Option 1
+
+- Ejecutar los casos de acuerdo al archivo de pruebas
+
+```bash
+$ go test -v ./internal/products/
+=== RUN   TestUpdateNameGood
+--- PASS: TestUpdateNameGood (0.00s)
+=== RUN   TestUpdateNameBad
+--- PASS: TestUpdateNameBad (0.00s)
+=== RUN   TestGetAll
+--- PASS: TestGetAll (0.00s)
+PASS
+ok      github.com/ejercicio_2/internal/products        0.987s
+```
+
+---
+
---
+++ 0x13-testing_integration_test/ejercicio_1/internal/products/repository.go
@@ -0,0 +1,280 @@
+package products
+
+import (
+	"fmt"
+        "errors"
+	"github.com/ejercicio_2/internal/domain"
+        "github.com/ejercicio_2/pkg/store"
+)
+
+type Repository interface {
+	GetAll() ([]domain.Product, error)
+	GetOne(id int) (domain.Product, error)
+	Store(name, color, code string, stock int, price float64, publish *bool) (domain.Product, error)
+	FilterProduct(name, color, code string, id, stock int, price float64, publish *bool) ([]*domain.Product, error)
+	Update(name, color, code string, id, stock int, price float64, publish *bool) (domain.Product, error)
+	UpdateStock(id int, stock int) (domain.Product, error)
+	UpdateName(id int, name string) (domain.Product, error)
+	Delete(id int) error
+}
+
+type repository struct{
+        db store.Store
+}
+
+
+func NewRepository(db store.Store) Repository {
+	return &repository{
+                db: db,
+        }
+}
+
+var (
+	// products []domain.Product
+	// lastID   int = 0
+)
+
+ 
+
+// Get one product
+func (r *repository) GetOne(id int) (product domain.Product, err error) {
+
+        var products []domain.Product
+        find := false
+        
+        err = r.db.Read(&products)
+        if err != nil {
+                return product, err
+        }
+
+        for _, p := range products {
+                if p.Id == id {
+                        find = true
+                        product = p
+                        break
+                 }
+        }
+      
+        if !find {
+                return product, errors.New("Not Found Product") 
+        }
+
+        return product, nil
+}
+
+
+// Get products
+func (r *repository) GetAll() (products []domain.Product, err error) {
+
+        err = r.db.Read(&products)
+        if err != nil {
+                return nil, err
+        }
+
+	return products, nil
+}
+
+// Store de products
+func (r *repository) Store(name, color, code string, stock int, price float64, publish *bool) (product domain.Product, err error) {
+
+        var products []domain.Product
+        var lastID int
+
+        err = r.db.Read(&products)
+        if err != nil {
+                return product, err
+        }
+
+	p := domain.Product{
+		Name: name,
+		Color: color,
+		Code: color,
+		Stock: stock,
+		Price: price,
+		Publish: publish,
+	}
+
+	if len(products) == 0 {
+		lastID = 0 
+	} else {
+	        lastID = products[len(products) - 1].Id
+        }
+
+	lastID++
+	p.Id = lastID
+	products = append(products, p)
+
+        err = r.db.Write(products)
+        if err != nil {
+                return product, err
+        }
+
+	return p, nil
+}
+
+// Filter product
+func (r *repository) FilterProduct(name, color, code string, id, stock int, price float64, publish *bool) (products []*domain.Product, err error) {
+
+        var filtrado []*domain.Product
+
+        err = r.db.Read(&products)
+        if err != nil {
+                return products, err
+        }
+
+        for _, t := range products { // filtrado por todos los campos
+                if id == t.Id &&
+                   stock == t.Stock &&
+                   name == t.Name &&
+                   color== t.Color &&
+                   code == t.Code &&
+                   price == t.Price &&
+                   *publish == *t.Publish { 
+                        filtrado = append(filtrado, t)
+                   }
+        }
+        
+	return filtrado, nil 
+}
+
+
+// Update product
+func (r *repository) Update(name, color, code string, id, stock int, price float64, publish *bool) (product domain.Product, err error) {
+
+        var products []domain.Product
+
+        err = r.db.Read(&products)
+        if err != nil {
+                return product, err
+        }
+
+	product = domain.Product{
+		Name: name,
+		Color: color,
+		Code: color,
+		Stock: stock,
+		Price: price,
+		Publish: publish,
+	}
+
+	var updated bool
+	for i := range products {
+		if products[i].Id == id {
+			product.Id = products[i].Id
+			products[i] = product
+			updated = true
+		}
+	}
+
+	if !updated {
+		return domain.Product{}, fmt.Errorf("product id %d not exists", id)
+	}
+        
+        err = r.db.Write(products)
+        if err != nil {
+                return product, err
+        }
+
+	return product, nil
+}
+
+
+
+// Update Name product
+func (r *repository) UpdateStock(id int, stock int) (product domain.Product, err error) {
+	var updated bool
+        var products []domain.Product
+
+        err = r.db.Read(&products)
+        if err != nil {
+                return product, err
+        }
+
+	product = domain.Product{}
+	for i := range products {
+		if products[i].Id == id {
+			products[i].Stock = stock
+			product = products[i]
+			updated = true
+		}
+	}
+
+	if !updated {
+		return domain.Product{}, fmt.Errorf("product id %d not exists", id)
+	}
+
+        err = r.db.Write(products)
+        if err != nil {
+                return product, err
+        }
+
+        return product, nil
+}
+
+
+// Update Name product
+func (r *repository) UpdateName(id int, name string) (product domain.Product, err error) {
+	var updated bool
+        var products []domain.Product
+
+        err = r.db.Read(&products)
+        if err != nil {
+                return product, err
+        }
+
+	product = domain.Product{}
+	for i := range products {
+		if products[i].Id == id {
+			products[i].Name = name 
+			product = products[i]
+			updated = true
+		}
+	}
+
+	if !updated {
+		return domain.Product{}, fmt.Errorf("product id %d not exists", id)
+	}
+
+        err = r.db.Write(products)
+        if err != nil {
+                return product, err
+        }
+
+        return product, nil
+}
+
+
+
+// Delete product
+func (r *repository) Delete(id int) error {
+	var deleted bool
+	var pos int
+
+        var products []domain.Product
+
+        err := r.db.Read(&products)
+        if err != nil {
+                return err
+        }
+
+	for i := range products {
+		if products[i].Id == id {
+			pos = i
+			deleted = true
+		}
+	}
+
+	if !deleted {
+		return fmt.Errorf("product id %d not exists", id)
+	}
+
+	products = append(products[:pos], products[pos + 1:]...)
+
+        err = r.db.Write(products)
+        if err != nil {
+                return err
+        }
+        
+	return nil
+}
+
